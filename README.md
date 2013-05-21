2564f1d63d8fa448e36b1946ca7613300f24d253
wvbe <wybe@x-54.com>
2013-05-22 00:23:03 +0200

    Initial code release, barely tested I've ran the code without errors by defining some simple send and receive callbacks, which did not use an actual API. Therefore, the script is not very much tested.

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
-beardbash
-=========
+BEARDBASH
+=============
+This is a HTML/Javascript package that emulates a terminal window. It tries to catch any and all keyboard input and is able to send it to an API as a string. Usually, you'll want to echo the response.
 
-BEEEEEEAAAAARDDDDDBASSSSSSSHHHHHHHHHH
+The functions to send to the API and receive from the API are defined in the second and third argument of the BeardBash class constructor.
+
+License
+-------
+This work is licensed under a Creative Commons CC BY-NC-SA 3.0 license. In short:
+
+* __Attribution__ — You must attribute the work in the manner specified by the author or licensor (but not in any way that suggests that they endorse you or your use of the work).
+* __Noncommercial__ — You may not use this work for commercial purposes.
+* __Share Alike__ — If you alter, transform, or build upon this work, you may distribute the resulting work only under the same or similar license to this one.
+
+The code in this repository was written by Wybe Minnebo (wybe@x-54.com) from http://www.x-54.com. Please leave any existing commented sections with this name and URL in place.
+
+For more information: http://creativecommons.org/licenses/by-nc-sa/3.0/
+
---
+++ js/beardbash.js
@@ -0,0 +1,137 @@
+function BeardBash(selector, send_callback, receive_callback) {
+    this.selector = selector;
+    this.cursorspeed = 500;
+    this.cursortimer = false;
+    this.always_focus = false;
+    this.mode = 'off';
+    this.send_callback = send_callback;
+    if(receive_callback==undefined)
+        this.receive_callback = this.output;
+    else
+        this.receive_callback = receive_callback;
+    
+    this.bindclicks = function() {
+        workaround = this;
+        document.addEventListener('click', function() { workaround.focus(); });
+        document.removeEventListener('click');
+    
+        $(document).on('click', this.selector+' a', function(e) {
+            input = $(this).data('input');
+            if($(this).hasClass('no-takeover')) {
+
+            } else if(input) {		
+                    e.preventDefault();
+                    workaround.send(input);
+            } else {
+                    e.preventDefault();
+                    workaround.send('/redirect '+$(this).attr('href'));
+            }
+        });
+    }
+    this.bindpresses = function() {
+        workaround = this;
+        
+        document.addEventListener('keyup',function(e) {
+            workaround.focus();
+            if(e.keyCode==13) { // [enter]
+                if(workaround.mode=='off') return;
+                workaround.send();
+            }
+        });
+        document.removeEventListener('keyup');
+
+
+        
+        $(this.selector+' .type').bind('input', function(e) {
+            if(workaround.mode=='off') return;
+            $(workaround.selector+' .faux_input').html($(this).val()
+                .replace(/&/g, "&amp;")
+                .replace(/</g, "&lt;")
+                .replace(/>/g, "&gt;")
+                .replace(/"/g, "&quot;")
+                .replace(/'/g, "&#039;")); // get the current value of the input field.
+            workaround.mode = 'start';
+            workaround.blink();
+        });
+    }
+    
+    this.send = function(input) {
+        if(input==undefined)
+            input = $(this.selector+' .faux_input').html();
+        if(input=='') return;
+        this.mode = 'off';
+        this.blink();
+        if(input=='/clear') $(this.selector+' .output-wrapper').html('');
+        $(this.selector+' .output-wrapper').append('<div class="line input"><label class="prefix">&#62;</label><p>'+input+'</p></div>');
+        $(this.selector+' .type').val('');
+        $(this.selector+' .faux_input').html('');
+        if(input=='/clear') {
+            this.resume();
+        } else {
+            this.send_callback(input);
+        }
+
+    }
+    this.receive = function(response) {
+        this.mode = 'start';
+        this.blink();
+        this.output(response);
+        this.receive_callback(response);
+    }
+    
+    this.output = function(m) {
+            $(this.selector+' .output-wrapper').append('<div class="line output"><label class="prefix">&nbsp;</label><p>'+m+'</p></div>');
+    }
+
+    this.blink = function() {
+        el = $(this.selector+' .cursor');
+        if(this.mode=='on') {
+            el.show();
+            window.clearTimeout(this.cursortimer);
+        } else if(this.mode=='off') {
+            el.hide();
+            window.clearTimeout(this.cursortimer);
+        } else if(this.mode=='blink') {
+            el.toggle();
+            workaround = this;
+            workaround.cursortimer = window.setTimeout(function() { workaround.blink(); }, this.cursorspeed);
+        } else if(this.mode=='start') {
+            el.show();
+            this.mode = 'blink';
+            workaround = this;
+            window.clearTimeout(workaround.cursortimer);
+            workaround.cursortimer = window.setTimeout(function() { workaround.blink(); }, this.cursorspeed);
+        }
+    }
+    this.resume = function() {
+        $(this.selector+' .type').val('');
+        $(this.selector+' .faux_input').html('');
+        this.mode = 'start';
+        this.blink();
+        this.focus();
+        window.scrollTo(0, document.body.scrollHeight);
+    }
+
+    this.focus = function() {
+        $(this.selector+' .type').focus();
+    }
+    this.escape = function(text) {
+        return text
+            .replace(/&/g, "&amp;")
+            .replace(/</g, "&lt;")
+            .replace(/>/g, "&gt;")
+            .replace(/"/g, "&quot;")
+            .replace(/'/g, "&#039;");
+    }
+    this.start = function() {
+        this.blink();
+        this.bindclicks();
+        this.bindpresses();
+        workaround = this;
+        window.setTimeout(function() {
+            workaround.output('Ready, welcome');
+            workaround.send('/connect x-54.com');
+        }, 780);
+        this.focus();
+    }
+}
